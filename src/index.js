8e677b9e927bc32b4ceb4b63eb4e3371125b16b4
faustinaodetaa <faustinaodeta@gmail.com>
2022-01-04 01:39:39 +0700

    5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const { app, BrowserWindow, Tray } = require("electron");
+// const { app, BrowserWindow, Tray } = require("electron");
+// const path = require("path");
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
+const url = require("url");
+const Tray = electron.Tray;
+const iconPath = path.join(__dirname, "bee.png");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -7,26 +15,34 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+// const createWindow = () => {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+//   // and load the index.html of the app.
+//   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-  // tray = new Tray("../bee.png");
-  // tray.setToolTip("bee-portal");
-};
+//   // Open the DevTools.
+//   mainWindow.webContents.openDevTools();
+//   tray = new Tray("bee.png");
+//   tray.setToolTip("bee-portal");
+// };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", function () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  new Tray(iconPath);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -41,7 +57,7 @@ app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    // createWindow();
   }
 });
 
