75d3312c2bc03c2d02fff94a883fdc314d1a84ea
faustinaodetaa <faustinaodeta@gmail.com>
2022-01-07 22:13:06 +0700

    6

--- src/index.js
+++ src/index.js
@@ -1,65 +1,57 @@
-// const { app, BrowserWindow, Tray } = require("electron");
-// const path = require("path");
-
 const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const url = require("url");
-const Tray = electron.Tray;
-const iconPath = path.join(__dirname, "bee.png");
+var { app, BrowserWindow, Tray, Menu } = require("electron");
+var path = require("path");
+var url = require("url");
+var iconPath = path.join(__dirname, "bee.png");
+let tray = null;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
-}
-
-// const createWindow = () => {
-//   // Create the browser window.
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//   });
-
-//   // and load the index.html of the app.
-//   mainWindow.loadFile(path.join(__dirname, "index.html"));
-
-//   // Open the DevTools.
-//   mainWindow.webContents.openDevTools();
-//   tray = new Tray("bee.png");
-//   tray.setToolTip("bee-portal");
-// };
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", function () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: iconPath,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
-  new Tray(iconPath);
-});
+  mainWindow.on("minimize", () => {
+    if (tray) {
+      return mainWindow.hide();
+    }
+    tray = new Tray(iconPath);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+    const template = [
+      {
+        label: "Show",
+        click: function () {
+          mainWindow.show();
+        },
+      },
+      {
+        label: "Maximize",
+        click: function () {
+          mainWindow.maximize();
+        },
+      },
+      {
+        label: "Minimize",
+        click: function () {
+          mainWindow.minimize();
+        },
+      },
+      {
+        label: "Exit",
+        click: function () {
+          mainWindow.close();
+        },
+      },
+    ];
 
-app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    // createWindow();
-  }
+    const contextMenu = Menu.buildFromTemplate(template);
+    tray.setContextMenu(contextMenu);
+    tray.setToolTip("bee-portal");
+    mainWindow.hide();
+  });
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
